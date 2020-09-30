ac2c9a989292dc251691d69ce25deee7888e9cf6
fridthoy <hoyer1996@hotmail.com>
2020-09-30 11:13:02 +0200

    add pickle

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppeCSV\\Donor_File_Contacts.csv', sep=';')
 
-
+df.to_pickle('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppe\\pickle_files\\Donor_File_Contacts.pkl')
 print(df)
