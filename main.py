772b2c3767549713210cabbb6555ebbbe7679dac
fridthoy <hoyer1996@hotmail.com>
2020-09-30 13:20:12 +0200

    some picklefiles added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppeCSV\\Donor_File_Contacts.csv', sep=';')
+df = pd.read_csv('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppeCSV\\Payment_File_Pledge.csv', sep=';')
 
-df.to_pickle('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppe\\pickle_files\\Donor_File_Contacts.pkl')
+#df.to_pickle('C:\\Users\\Fridtjof\\Documents\\skole høst 2020\\dataAnalyse\\gruppe\\pickle_files\\Pledge_File_Contacts.pkl')
 print(df)
